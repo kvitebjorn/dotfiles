f4e24d862b11688ecf63640bf411591a7606781f
kvitebjorn <harintonkairu@gmail.com>
2023-12-27 21:11:54 -0700

    update emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -13,7 +13,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rainbow-identifiers rainbow-delimiters rainbow-blocks monokai-pro-theme)))
+   '(rainbow-identifiers rainbow-delimiters rainbow-blocks monokai-pro-theme))
+ '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
