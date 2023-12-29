2f65685c65f9024e94b6bd8754d1954dbdf402df
kvitebjorn <harintonkairu@gmail.com>
2023-12-29 14:46:12 -0700

    change font

--- emacs/init.el
+++ emacs/init.el
@@ -27,7 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "CaskaydiaCove Nerd Font Mono" :foundry "SAJA" :slant normal :weight normal :height 128 :width normal)))))
+ '(default ((t (:family "JuliaMono" :foundry "corm" :slant normal :weight normal :height 113 :width normal)))))
 
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
