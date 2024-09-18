79d3be5fc9cccc96d9e8de8e3a4960d70c0d73eb
kyle <kyle@localhost.localdomain>
2024-09-17 22:22:48 -0600

    update nvim and tmux dotfiles

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -26,5 +26,5 @@ end, { desc = "Debug last go test" })
 -- tmux overrides
 map("n", "<C-h>", "<cmd> TmuxNavigateLeft <CR>", { desc = "window left" })
 map("n", "<C-l>", "<cmd> TmuxNavigateRight <CR>", { desc = "window right" })
-map("n", "<C-j>", "<cmd> TmuxNavigateBottom <CR>", { desc = "window down" })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown <CR>", { desc = "window down" })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp <CR>", { desc = "window up" })
